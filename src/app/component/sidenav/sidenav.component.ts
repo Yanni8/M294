f6f48c03c16a1168645ebdc1590def0fbb2c6638
Yannick Müller <mail@ynnk.dev>
2023-06-02 10:48:43 +0200

    update login flow

--- src/app/component/sidenav/sidenav.component.ts
+++ src/app/component/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppAuthService } from 'src/app/service/app.auth.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidenav.component.less']
 })
 export class SidenavComponent  {
+
+  constructor (private authService: AppAuthService){}
+
+  get isLogedIn(){
+    return this.authService.isAuthenticated();
+  }
+
+  isHome(){
+    return window.location.pathname === "/home";
+  }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { User } from 'src/app/model/user.model';
+import { AppAuthService } from 'src/app/service/app.auth.service';
+import { selectCurrentUser } from 'src/app/stage/user/user.selector';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  public user: User | null = null;
+
+  constructor(private authService: AppAuthService, private store: Store){}
+
+  get isLogedIn(){
+    return this.authService.isAuthenticated();
+  }
+
+  ngOnInit(): void {
+    this.store.select(selectCurrentUser).subscribe(user => this.user = user || null);
+  }
 }
